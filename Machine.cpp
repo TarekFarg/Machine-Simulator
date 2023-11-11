d7e5700be1f6126a803a71ee922a8a905a31ce1d
Mahmoud3177Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2023-11-11 19:24:08 +0200

    Update Machine.cpp

--- Machine.cpp
+++ Machine.cpp
@@ -1,1 +1,24 @@
-#include "Machine.h"
+#include<iostream>
+#include "machine.h"
+//#include"instructions.h"
+
+void Machine::load_1(Instructions s)
+{
+    Register[s.index_R] = memory[s.index_1M][s.index_2M];
+}
+
+void Machine::load_2(Instructions s)
+{
+    Register[s.index_R] = s.decimal_number;
+}
+
+void Machine::store_3(Instructions s)
+{
+    if(s.index_1M == 0 && s.index_2M == 0)
+    {
+        std::cout << Register[s.index_R] << " ";
+        return;
+    }
+
+    memory[s.index_1M][s.index_2M] = Register[s.index_R];
+}
