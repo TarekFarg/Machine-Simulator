936201f67091cee052db070adaa87c1c1c37cfcf
Mahmoud3177Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2023-11-11 19:24:24 +0200

    Update Machine.h

--- Machine.h
+++ Machine.h
@@ -1,20 +1,29 @@
-#pragma once
+//#pragma once
+//#ifndef MAC
+//#define MAC
 #include <vector>
+#include"instructions.h"
+
 using namespace std;
+
 class Machine
 {
-private:
-	int memory[20][20];
-	int Register[20];
-public:
-	void load();
-	void load_2();
-	void store();
-	void move();
-	int add();
-	double add_2();
-	void jump();
+    private:
+
+    public:
+        int memory[20][20];
+        int Register[20];
+        void load_1(Instructions s);
+        void load_2(Instructions s);
+        void store_3(Instructions s);
+        void print_3();
+        void move_4();
+        int add_5();
+        double add_6();
+        void jump_B();
+        void exit_C();
 
 
 };
 
+//#endif
