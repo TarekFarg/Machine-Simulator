f18cfca750dd8564581e7a43e6c4412808c34db9
Mahmoud3177Khaled <124203317+Mahmoud3177Khaled@users.noreply.github.com>
2023-11-14 18:43:43 +0200

    Update Machine.h

--- Machine.h
+++ Machine.h
@@ -18,7 +18,7 @@ class Machine
         void store_3(Instructions s);
         void move_4(Instructions s);
         void add_5(Instructions s);
-        int jump_B(Instructions s);
+        int jump_B(Instructions s, int i);
         int exit_C(Instructions s);
 
 
